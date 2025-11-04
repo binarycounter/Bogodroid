bbf980a0eafddb424389790ae02c56c9a311c0c3
binarycounter <binarycountermusic@gmail.com>
2025-11-04 22:43:48 +0100

    GPlay: Add PlayGamesSdk and ReviewManager

---
+++ javastubs/gplay.cpp
@@ -0,0 +1,112 @@
+#include "gplay.h"
+#include "logging.h"
+
+std::shared_ptr<jnivm::com::google::android::play::core::review::ReviewManager> jnivm::com::google::android::play::core::review::ReviewManagerFactory::create(std::shared_ptr<jnivm::android::content::Context> context)
+{
+    return nullptr;
+}
+
+std::shared_ptr<jnivm::com::google::android::gms::games::AchievementsClient> jnivm::com::google::android::gms::games::PlayGames::getAchievementsClient(std::shared_ptr<jnivm::android::app::Activity> activity)
+{
+    return nullptr;
+}
+
+std::shared_ptr<jnivm::com::google::android::gms::games::EventsClient> jnivm::com::google::android::gms::games::PlayGames::getEventsClient(std::shared_ptr<jnivm::android::app::Activity> activity)
+{
+    return nullptr;
+}
+
+std::shared_ptr<jnivm::com::google::android::gms::games::GamesSignInClient> jnivm::com::google::android::gms::games::PlayGames::getGamesSignInClient(std::shared_ptr<jnivm::android::app::Activity> activity)
+{
+    return nullptr;
+}
+
+std::shared_ptr<jnivm::com::google::android::gms::games::LeaderboardsClient> jnivm::com::google::android::gms::games::PlayGames::getLeaderboardsClient(std::shared_ptr<jnivm::android::app::Activity> activity)
+{
+    return nullptr;
+}
+
+std::shared_ptr<jnivm::com::google::android::gms::games::PlayerStatsClient> jnivm::com::google::android::gms::games::PlayGames::getPlayerStatsClient(std::shared_ptr<jnivm::android::app::Activity> activity)
+{
+    return nullptr;
+}
+
+std::shared_ptr<jnivm::com::google::android::gms::games::PlayersClient> jnivm::com::google::android::gms::games::PlayGames::getPlayersClient(std::shared_ptr<jnivm::android::app::Activity> activity)
+{
+    return nullptr;
+}
+
+std::shared_ptr<jnivm::com::google::android::gms::games::SnapshotsClient> jnivm::com::google::android::gms::games::PlayGames::getSnapshotsClient(std::shared_ptr<jnivm::android::app::Activity> activity)
+{
+    return nullptr;
+}
+
+void jnivm::com::google::android::gms::games::PlayGamesSdk::initialize(std::shared_ptr<jnivm::android::content::Context> context)
+{
+    // Initialize the Play Games SDK with the provided context
+}
+
+BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::play::core::review::ReviewManager) { FakeJni::Constructor<ReviewManager> {} },
+    END_NATIVE_DESCRIPTOR
+
+BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::play::core::review::ReviewManagerFactory) { FakeJni::Constructor<ReviewManagerFactory> {} },
+    { FakeJni::Function<&ReviewManagerFactory::create> {}, "create", FakeJni::JMethodID::STATIC },
+    END_NATIVE_DESCRIPTOR
+
+
+BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::gms::games::AchievementsClient) { FakeJni::Constructor<AchievementsClient> {} },
+    END_NATIVE_DESCRIPTOR
+
+    BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::gms::games::EventsClient) { FakeJni::Constructor<EventsClient> {} },
+    END_NATIVE_DESCRIPTOR
+
+    BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::gms::games::GamesSignInClient) { FakeJni::Constructor<GamesSignInClient> {} },
+    END_NATIVE_DESCRIPTOR
+
+    BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::gms::games::LeaderboardsClient) { FakeJni::Constructor<LeaderboardsClient> {} },
+    END_NATIVE_DESCRIPTOR
+
+    BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::gms::games::PlayerStatsClient) { FakeJni::Constructor<PlayerStatsClient> {} },
+    END_NATIVE_DESCRIPTOR
+
+    BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::gms::games::PlayersClient) { FakeJni::Constructor<PlayersClient> {} },
+    END_NATIVE_DESCRIPTOR
+
+    BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::gms::games::SnapshotsClient) { FakeJni::Constructor<SnapshotsClient> {} },
+    END_NATIVE_DESCRIPTOR
+
+    BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::gms::games::PlayGames) {
+        FakeJni::Constructor<PlayGames> {},
+    },
+    { FakeJni::Function<&PlayGames::getAchievementsClient> {}, "getAchievementsClient", FakeJni::JMethodID::STATIC },
+    { FakeJni::Function<&PlayGames::getEventsClient> {}, "getEventsClient", FakeJni::JMethodID::STATIC },
+    { FakeJni::Function<&PlayGames::getGamesSignInClient> {}, "getGamesSignInClient", FakeJni::JMethodID::STATIC },
+    { FakeJni::Function<&PlayGames::getLeaderboardsClient> {}, "getLeaderboardsClient", FakeJni::JMethodID::STATIC },
+    { FakeJni::Function<&PlayGames::getPlayerStatsClient> {}, "getPlayerStatsClient", FakeJni::JMethodID::STATIC },
+    { FakeJni::Function<&PlayGames::getPlayersClient> {}, "getPlayersClient", FakeJni::JMethodID::STATIC },
+    { FakeJni::Function<&PlayGames::getSnapshotsClient> {}, "getSnapshotsClient", FakeJni::JMethodID::STATIC },
+    END_NATIVE_DESCRIPTOR
+
+    BEGIN_NATIVE_DESCRIPTOR(jnivm::com::google::android::gms::games::PlayGamesSdk)
+{
+    FakeJni::Constructor<PlayGamesSdk> {},
+},
+{ FakeJni::Function<&PlayGamesSdk::initialize> {}, "initialize", FakeJni::JMethodID::STATIC },
+END_NATIVE_DESCRIPTOR
+
+    void InitJNIGooglePlayClasses(FakeJni::Jvm* vm)
+{
+    verbose("JBRIDGE", "Initializing Play Games JNI Classes");
+    vm->registerClass<jnivm::com::google::android::play::core::review::ReviewManager>();
+    vm->registerClass<jnivm::com::google::android::play::core::review::ReviewManagerFactory>();
+
+    vm->registerClass<jnivm::com::google::android::gms::games::AchievementsClient>();
+    vm->registerClass<jnivm::com::google::android::gms::games::EventsClient>();
+    vm->registerClass<jnivm::com::google::android::gms::games::GamesSignInClient>();
+    vm->registerClass<jnivm::com::google::android::gms::games::LeaderboardsClient>();
+    vm->registerClass<jnivm::com::google::android::gms::games::PlayerStatsClient>();
+    vm->registerClass<jnivm::com::google::android::gms::games::PlayersClient>();
+    vm->registerClass<jnivm::com::google::android::gms::games::SnapshotsClient>();
+    vm->registerClass<jnivm::com::google::android::gms::games::PlayGames>();
+    vm->registerClass<jnivm::com::google::android::gms::games::PlayGamesSdk>();
+}
--- javastubs/gplay.h
+++ javastubs/gplay.h
@@ -1,16 +1,32 @@
 #ifndef __PLAYGAMES_H__
 #define __PLAYGAMES_H__
 
+#include "android.h"
 #include "baron/baron.h"
 #include "javac.h"
-#include "android.h"
 
-void InitJNIPlayGamesClasses(FakeJni::Jvm* vm);
+void InitJNIGooglePlayClasses(FakeJni::Jvm* vm);
 
 namespace jnivm {
 namespace com {
     namespace google {
         namespace android {
+            namespace play {
+                namespace core {
+                    namespace review {
+                        class ReviewManager : public FakeJni::JObject {
+                        public:
+                            DEFINE_CLASS_NAME("com/google/android/play/core/review/ReviewManager")
+                        };
+                        class ReviewManagerFactory : public FakeJni::JObject {
+                        public:
+                            DEFINE_CLASS_NAME("com/google/android/play/core/review/ReviewManagerFactory")
+                            static std::shared_ptr<ReviewManager> create(std::shared_ptr<jnivm::android::content::Context> context);
+                        };
+                    }
+                }
+            }
+
             namespace gms {
                 namespace games {
                     class AchievementsClient : public FakeJni::JObject {
@@ -58,6 +74,12 @@ namespace com {
                         static std::shared_ptr<jnivm::com::google::android::gms::games::PlayerStatsClient> getPlayerStatsClient(std::shared_ptr<jnivm::android::app::Activity> activity);
                         static std::shared_ptr<jnivm::com::google::android::gms::games::PlayersClient> getPlayersClient(std::shared_ptr<jnivm::android::app::Activity> activity);
                         static std::shared_ptr<jnivm::com::google::android::gms::games::SnapshotsClient> getSnapshotsClient(std::shared_ptr<jnivm::android::app::Activity> activity);
+                    };
+
+                    class PlayGamesSdk : public FakeJni::JObject {
+                    public:
+                        DEFINE_CLASS_NAME("com/google/android/gms/games/PlayGamesSdk")
+                        static void initialize(std::shared_ptr<jnivm::android::content::Context> context);
                     };
                 }
             }
